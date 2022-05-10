34c6e0ab7660496e83728a9efb3f0573d4551ba2
Tulika1983 <87574734+Tulika1983@users.noreply.github.com>
2022-05-10 15:36:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,11 @@
 
 function preload() {
 	world_start = loadSound("world_start.wav");
+	mario_jump = loadSound("jump.wav");
+	mario_coin = loadSound("coin.wav");
+	mario_gameover = loadSound("gameover.wav");
+	mario_die = loadSound("mariodie.wav");
+	mario_kick = loadSound("kick.wave");
 	setSprites();
 	MarioAnimation();
 }
